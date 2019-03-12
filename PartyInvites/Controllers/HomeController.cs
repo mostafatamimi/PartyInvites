dd8145a75442d7c56c0024acab049d6613d5f20a
mostafa <mostafatamimi@gmail.com>
2019-03-12 09:36:46 +0200

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ViewResult Index()
+        {
+            int Hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting = Hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            //return Content("Hello Class");
+            return HttpNotFound();
+        }
+
+        
+    }
+}
---
+++ PartyInvites/Controllers/PartyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class PartyController : Controller
+    {
+        PartyDbContext db = new PartyDbContext();
+        // GET: Party
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse model)
+        {
+            if (ModelState.IsValid)
+            {
+                var count= db.GuestResponses.Count();
+                model.ID = count + 1;
+                db.GuestResponses.Add(model);
+                db.SaveChanges();
+                return View("Thanks", model);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
